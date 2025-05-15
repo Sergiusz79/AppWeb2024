811bdf047eb2d67ea09fb5e165427c1b06822659
Sergiusz79 <mc.sergey30@gmail.com>
2025-05-15 13:25:36 +0200

    Task3_final

--- Project_4/Task3.js
+++ Project_4/Task3.js
@@ -1,6 +1,14 @@
 function ustawKolor()  
-{
-    color=document.getElementById("poleTekstowe").value;  
-    element=document.getElementById("akapit"); 
+{   
+    // значение, введённое пользователем в текстовое поле с id="poleTekstowe", преобразуется в целое число с помощью parseInt (с основанием системы счисления 10).
+    let color = parseInt(document.getElementById("poleTekstowe").value, 10);  
+    //Проверяется, является ли введённое значение числом (isNaN(color)), а также находится ли оно в диапазоне от 0 до 255. Если значение некорректно, появляется предупреждение, и выполнение функции прерывается.
+    if (isNaN(color) || color < 0 || color > 100) {
+        alert("Wprowadź liczbę z zakresu 0-100!");
+        return;
+    }
+    //Объявляем переменную element и присваиваем ей значение 
+    let element=document.getElementById("Block1");
+    //Изменяем стиль цвета backgroung-color у элемента element 
     element.style.backgroundColor="rgb(" + color + "," + color + "," + color + ")";
 }
